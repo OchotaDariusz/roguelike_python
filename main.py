f19938eed54ff31393e43d0fd1953a97a1a43392
Maciej Strękowski <maciejstrekowski@gmail.com>
2022-03-16 14:18:02 +0100

    Add Multiple maps functionality

--- main.py
+++ main.py
@@ -51,14 +51,21 @@ def main():
     #SYLWEK# print(player)
     #SYLWEK# print(engine.show_inventory(player))
     #SYLWEK# input()
+    for level_number in range(1,4):
+        level_file = "level_"+str(level_number)+".txt"
+        board = engine.create_board(BOARD_WIDTH, BOARD_HEIGHT)
+        engine.export_board(board,level_file)
+    level_number = [1]
 
-    board = engine.create_board(BOARD_WIDTH, BOARD_HEIGHT)
+    #board = engine.create_board(BOARD_WIDTH, BOARD_HEIGHT)
     board[5][5] = "M"
     board[7][7] = "I"
     # board[10][10] = "#"
     util.clear_screen()
     is_running = True
     while is_running:
+        level_file = "level_"+str(level_number[0])+".txt"
+        board = engine.import_bord(level_file)
         engine.put_player_on_board(board, player)
         ui.display_board(board)
         print(player)
@@ -73,7 +80,7 @@ def main():
             player_movement.step_direction(player, key, board)
         board[backup_pos_x][backup_pos_y] = '.'
         util.clear_screen()
-        engine.event_handler(player, board)
+        engine.event_handler(player, board,level_number)
         if player["lives"] <= 0:
             input("Game Over!")
             sys.exit()
